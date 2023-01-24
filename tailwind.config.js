f62e6a0d2f89552ce15cec61cc3726de522a1535
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-01-24 14:37:31 +0600

    added banner carousel and legal practice section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garamond: ["EB Garamond", "serif"],
+      },
+      backgroundImage: {
+        "card-gradient":
+          "linear-gradient(180deg, #F3F3F3 0%, rgba(243, 243, 243, 0) 100%)",
+      },
+    },
   },
   plugins: [],
-}
+};
